2a9fd38832d3bc97f1d4aa610c21bf2ec505e7a0
Caitlin Sedwick <cait@cait-albook-5.local>
2018-08-22 13:44:25 -0700

    Convert to MVC design pattern

---
+++ TodoeyToo/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  TodoeyToo
+//
+//  Created by Caitlin Sedwick on 8/22/18.
+//  Copyright © 2018 Caitlin Sedwick. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
